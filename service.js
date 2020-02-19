6534d3059a9bfcf243e9f4ab7c20158760939ee2
khalid saaf <khallid.saaf@gmail.com>
2020-02-19 15:15:47 +0100

    mise a jour
    ajouter animation jquery

---
+++ service.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal('.item1', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.item4', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item6', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item8', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item10', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item12', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
